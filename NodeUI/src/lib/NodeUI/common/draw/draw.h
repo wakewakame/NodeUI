50a0ee93c98b79e5a4edb1de56d81293d0c2e2a8
吉井 大輝 <0214.taiki@gmail.com>
2017-09-09 17:39:16 +0900

    ちょい改善

--- NodeUI/src/lib/NodeUI/common/draw/draw.h
+++ NodeUI/src/lib/NodeUI/common/draw/draw.h
@@ -4,6 +4,18 @@
 
 namespace nui {
 	class Draw : public DrawBase {
-
+	public:
+		static void line(float left, float top, float right, float bottom) {
+			nui::Draw::addPoint(left, top);
+			nui::Draw::addPoint(right, bottom);
+			nui::Draw::drawPoint(GL_LINES);
+		}
+		static void box(float left, float top, float right, float bottom) {
+			nui::Draw::addPoint(left, top);
+			nui::Draw::addPoint(right, top);
+			nui::Draw::addPoint(right, bottom);
+			nui::Draw::addPoint(left, bottom);
+			nui::Draw::drawPoint(GL_TRIANGLE_FAN);
+		}
 	};
 }
--- NodeUI/src/main.cpp
+++ NodeUI/src/main.cpp
@@ -13,16 +13,14 @@ int main() {
 
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-		nui::Draw::setColor(1.0f, 0.0f, 0.0f, 1.0f);
-		nui::Draw::addPoint(0.0f, 0.0f);
-		nui::Draw::setColor(0.0f, 1.0f, 0.0f, 1.0f);
-		nui::Draw::addPoint((float)nui::Window::getSize().x, 0.0f);
-		nui::Draw::setColor(0.0f, 0.0f, 1.0f, 1.0f);
-		nui::Draw::addPoint((float)nui::Window::getSize().x - 100.0f, (float)nui::Window::getSize().y - 100.0f);
-		nui::Draw::setColor(1.0f, 1.0f, 0.0f, 1.0f);
-		nui::Draw::addPoint(0.0f, (float)nui::Window::getSize().y);
+		nui::Draw::setColor(0.0f, 0.5f, 0.78f, 1.0f);
+		nui::Draw::box(
+			nui::Mouse::getCursor().x - 100,
+			nui::Mouse::getCursor().y - 100,
+			nui::Mouse::getCursor().x + 100,
+			nui::Mouse::getCursor().y + 100
+		);
 
-		nui::Draw::addPoint((float)nui::Window::getSize().x, (float)nui::Window::getSize().y);
 
 		nui::Draw::drawPoint(GL_LINE_STRIP);
 
